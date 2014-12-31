88b0b8c3c0de39b99d0558d20b714b9878412ded
Patrick Xu <xu.17@dartmouth.edu>
2014-12-31 10:07:37 -0500

    two-player functionality

--- mancalamaster.py
+++ mancalamaster.py
@@ -10,6 +10,11 @@ class mancala:
     self.board = {'0': 0, '1': 4, '2': 4, '3': 4, '4': 4, '5': 4, '6': 4,
                     '7': 0, '8': 4, '9': 4, '10': 4, '11': 4, '12': 4, '13': 4}
 
+  """setup the game"""
+  def setup():
+    """options"""
+    print
+
   def printBoard(self):
     print "\n\t", 
     for x in range(1,7): print self.board[str(7-x)],
@@ -19,54 +24,61 @@ class mancala:
     for x in range(1,7): print self.board[str(x+7)], 
     print "\n"
 
-  def getMove(self, playerNum): #TODO: add player array as argument
+  """takes in the current player number as an argument"""
+  def getMove(self, playerNum):
+    print "It's player" + str(playerNum+1) + "'s turn!"
+
+    """get upper and lower bound on valid moves based on current player"""
+    moveLower = 1 + 7*playerNum
+    moveUpper = 6 + 7*playerNum
+
     """get valid user input"""
     while True: 
       try:
         move = int(input("Enter a position to move: "))
-        if move < 1 or move > 6:
+        if move < moveLower or move > moveUpper:
           raise ValueError
         break
       except (NameError, SyntaxError, ValueError):
-        print "Please enter a position between 1 and 6.\n"
+        print "Please enter a position between {0} and {1}.\n".format(moveLower, moveUpper)
     self.executeMove(move)
   
+  """given a position on the board to be moved, make that move"""
   def executeMove(self, move):
     val = self.board[str(move)]
     self.board[str(move)] = 0
     x = 1
     while x < val + 1:
-      x += 1
       """get the pit to place a seed into"""
       current = (move + x) % 14
       """check if we would be placing into opponent's end pit"""
       if move < 7 and current == 0:
         val += 1
+        x += 1
         continue
       if move > 7 and current == 7:
         val += 1
+        x += 1
         continue
 
-      print "current: ", current
       """check if the last seed is being placed into an empty pit on our side"""
       if x == val and self.board[str(current)] == 0:
         if move < 7:
           self.board['7'] += 1 + self.board[str(14-current)]
           self.board[str(14-current)] = 0
-          print str(14-current)
         elif move > 7:
           self.board['0'] += 1 + self.board[str(14-current)]
           self.board[str(14-current)] = 0
+        x += 1
         continue
 
       """add one to the pit"""
       self.board[str(current)] += 1
+      x += 1
 
-    #for x in range(0,14): print x, self.board[str(x)], "  ",
-    #print ""
 
+  """check that both players have pieces left"""
   def gameOver(self):
-    """check that both players have pieces left"""
     for x in range(1,7):
       current = str(x)
       if self.board[current] != 0: return False 
@@ -92,8 +104,11 @@ class mancala:
 if __name__ == '__main__':
 
   game = mancala()
+  game.setup()
   game.printBoard()
+  turnNum = 0
   while game.gameOver() == False:
-    game.getMove(1)
+    game.getMove(turnNum%2)
     game.printBoard()
+    turnNum += 1
   game.getWinner()
